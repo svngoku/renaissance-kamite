a38ab160f399ea37d3a53ca25f525837f4bc121c
MartyNio <marymarty104@gmail.com>
2020-11-25 19:05:27 +0100

    updating thinks

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,10 +4,9 @@ import style from './style';
 
 const Header = () => (
 	<header class={style.header}>
-		<Link href="/"><h1>Jane Doe</h1></Link>
+		<Link href="/"><h1>Renaissance Kamite</h1></Link>
 		<nav>
-			<Link activeClassName={style.active} href="/blogs">Blogs</Link>
-			<Link activeClassName={style.active} href="/contact">Contact me</Link>
+			<Link activeClassName={style.active} href="/blogs">Blog</Link>
 		</nav>
 	</header>
 );
